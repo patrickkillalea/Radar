c1893fae095f9ff17ba269a5160e039c291501b0
unknown <AHU15@IRELFWZPK32.up.corp.upc>
2015-08-14 15:59:54 +0100

    Circles added that are draggable :tokyo_tower:

--- Radar/Scripts/Controllers/radarController.js
+++ Radar/Scripts/Controllers/radarController.js
@@ -1,8 +1,17 @@
 ﻿var controllerId = 'radarController';
 
-angular.module('app', []).controller(controllerId, ['$scope', radarController]);
+angular.module('app', []).controller(controllerId, ['$scope', '$timeout', radarController]);
 
-function radarController($scope) {
+function radarController($scope, $timeout) {
+    $scope.circles = [{ 'Name': 'Bootstrap', 'x': 100, 'y': 100 }, { 'Name': 'Bootstrap2', 'x': 10, 'y': 10 }]
+
+    $.each($scope.circles, function (index, circle) { 
+        $timeout(function() { //Move code up the callstack to tell Angular to watch this
+            $("#" + circle.Name).draggable();
+            $("#" + circle.Name).css({ top: circle.y, left: circle.x });
+        });
+    }); 
+
     /*
     $("#radar").draggable({
         // Find original position of dragged image.
