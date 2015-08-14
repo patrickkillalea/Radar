3bc7c1b5e52af41b7dbd299c4e506a094cba8948
unknown <AHU15@IRELFWZPK32.up.corp.upc>
2015-08-14 15:01:21 +0100

    Bootstrap now correctly added :umbrella:

--- Radar/Scripts/Controllers/radarController.js
+++ Radar/Scripts/Controllers/radarController.js
@@ -3,5 +3,20 @@
 angular.module('app', []).controller(controllerId, ['$scope', radarController]);
 
 function radarController($scope) {
+    /*
+    $("#radar").draggable({
+        // Find original position of dragged image.
+        start: function (event, ui) {
+            // Show start dragged position of image.
+            var Startpos = $(this).position();
+            console.log(Startpos);
+        },
+        // Find position where image is dropped.
+        stop: function (event, ui) {
 
+            // Show dropped position.
+            var Stoppos = $(this).position();
+            console.log(Stoppos);
+        }
+    });*/ 
 };
