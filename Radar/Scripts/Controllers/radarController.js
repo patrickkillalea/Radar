263ef960bac8c0baf61e1e12b4bafca6c17da828
unknown <AHU15@IRELFWZPK32.up.corp.upc>
2015-08-17 10:04:30 +0100

    Circles can now be added with name and position are recorded in array. Modal also added :cat:

--- Radar/Scripts/Controllers/radarController.js
+++ Radar/Scripts/Controllers/radarController.js
@@ -5,12 +5,48 @@ angular.module('app', []).controller(controllerId, ['$scope', '$timeout', radarC
 function radarController($scope, $timeout) {
     $scope.circles = [{ 'Name': 'Bootstrap', 'x': 100, 'y': 100 }, { 'Name': 'Bootstrap2', 'x': 10, 'y': 10 }]
 
-    $.each($scope.circles, function (index, circle) { 
-        $timeout(function() { //Move code up the callstack to tell Angular to watch this
-            $("#" + circle.Name).draggable();
-            $("#" + circle.Name).css({ top: circle.y, left: circle.x });
+    makeCirclesDraggable()
+
+    $('[data-toggle="tooltip"]').tooltip({
+        placement: 'top'
+    });
+
+    //FUNCTIONS -------------------------------------------------------------------------------------------------------------------------------------
+
+    $scope.createCircle = function () {
+        $scope.circles.push({ 'Name': $scope.circleName, 'x': 1000, 'y': 50 });
+        makeCirclesDraggable()
+    }
+
+    function makeCirclesDraggable() { 
+        $.each($scope.circles, function (index, circle) {
+
+            $timeout(function () { //Move code up the callstack to tell Angular to watch this 
+                //makes the circle draggable as well logging the position it is lifted from and where it is dropped to the console
+                $("#" + circle.Name).draggable({
+                    // Find original position of dragged image.
+                    start: function (event, ui) {
+                        // Show start dragged position of image.
+                        var Startpos = $(this).position();
+                        console.log(Startpos); 
+                    },
+                    // Find position where image is dropped.
+                    stop: function (event, ui) {
+
+                        // Show dropped position.
+                        var Stoppos = $(this).position();
+                        console.log(Stoppos);
+
+                        //updating it's location
+                        circle.x = Stoppos.left;
+                        circle.y = Stoppos.top;
+                    }
+                });
+                $("#" + circle.Name).css({ top: circle.y, left: circle.x });
+            });
+
         });
-    }); 
+    }
 
     /*
     $("#radar").draggable({
@@ -27,5 +63,5 @@ function radarController($scope, $timeout) {
             var Stoppos = $(this).position();
             console.log(Stoppos);
         }
-    });*/ 
+    });*/
 };
