0e1fe0e143d2d6eb26bc9e3cb67e1ff1c301d53d
unknown <AHU15@IRELFWZPK32.up.corp.upc>
2015-08-18 13:39:26 +0100

    You can now look at info for circle as well as edit it through the modal if necessary :spoon:

--- Radar/Scripts/Controllers/radarController.js
+++ Radar/Scripts/Controllers/radarController.js
@@ -5,6 +5,7 @@ angular.module('app', []).controller(controllerId, ['$scope', '$timeout', radarC
 function radarController($scope, $timeout) {
 
     var circleDiameter = 15;
+    $scope.clickedCircle = 'poop';
 
     $scope.circles = [{ 'Name': 'Bootstrap', 'x': 50, 'y': 50 }, { 'Name': 'Bootstrap2', 'x': 10, 'y': 10 }]
 
@@ -16,8 +17,22 @@ function radarController($scope, $timeout) {
 
     //FUNCTIONS -------------------------------------------------------------------------------------------------------------------------------------
 
-    $scope.createCircle = function () {
+    $scope.createCircle = function () { 
         $scope.circles.push({ 'Name': $scope.circleName, 'x': 0, 'y': 0, 'CircleType': $scope.circleType, 'Website': $scope.website });
+        makeCirclesDraggable()
+    }
+
+    $scope.editCircle = function () {
+        $scope.nameEdit = $scope.clickedCircle.Name;
+        $scope.typeEdit = $scope.clickedCircle.CircleType;
+        $scope.websiteEdit = $scope.clickedCircle.Website;
+    }
+
+    $scope.saveEdit = function () {
+        $scope.clickedCircle.Name = $scope.nameEdit;
+        $scope.clickedCircle.CircleType = $scope.typeEdit;
+        $scope.clickedCircle.Website = $scope.websiteEdit;
+
         makeCirclesDraggable()
     }
 
@@ -46,34 +61,38 @@ function radarController($scope, $timeout) {
                     }
                 });
 
-                calculateCirclePositions()
+                calculateCirclePositions(circle)
 
                 //creating a tooltip for each circle with it's name
-                Tipped.create("#" + circle.Name, circle.Name, { position: 'topleft' }); 
+                Tipped.create("#" + circle.Name, circle.Name, { position: 'topleft' });
             });
 
         });
     }
 
-    function calculateCirclePositions() {
-        $.each($scope.circles, function (index, circle) {
-            $timeout(function () { //Move code up the callstack to tell Angular to watch this  
+    function calculateCirclePositions(circle) {
+        $timeout(function () { //Move code up the callstack to tell Angular to watch this  
 
-                //using the size and location of the radar image as a reference to position the circles. 
-                var radarTopLeft = { 'x': $("#radar").position().left, 'y': $("#radar").position().top };
+            //using the size and location of the radar image as a reference to position the circles. 
+            var radarTopLeft = { 'x': $("#radar").position().left, 'y': $("#radar").position().top };
 
-                var circleX = radarTopLeft.x + (($("#radar").width() / 100) * circle.x);
-                var circleY = radarTopLeft.y + (($("#radar").height() / 100) * circle.y);
+            var circleX = radarTopLeft.x + (($("#radar").width() / 100) * circle.x);
+            var circleY = radarTopLeft.y + (($("#radar").height() / 100) * circle.y);
 
-                $("#" + circle.Name).css({ top: circleY, left: circleX });
+            $("#" + circle.Name).css({ top: circleY, left: circleX });
 
-                //Giving the circles a size depending on how big the window is
-                var circleSize = ($("#radar").width() / 722) * circleDiameter;
+            //Giving the circles a size depending on how big the window is
+            var circleSize = ($("#radar").width() / 722) * circleDiameter;
 
-                $("#" + circle.Name).css("width", circleSize);
-                $("#" + circle.Name).css("height", circleSize);
-            });
+            $("#" + circle.Name).css("width", circleSize);
+            $("#" + circle.Name).css("height", circleSize); 
         });
+    }
+
+    //opening modal on double click to display info about circle
+    $scope.openInfoModal = function (circle) { 
+        $scope.clickedCircle = circle;
+        $('#infoModal').modal('toggle'); 
     }
 
     //when the window resizes the circles will stay in the position they are meant to on the radar
@@ -84,6 +103,8 @@ function radarController($scope, $timeout) {
             });
         });
     });
+
+
 
 
     /*
